8214e7e2450fa99fa046370ee467ca26f87e9eb5
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-01 15:19:12 -0300

    feat : sparkles: criando context

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Hero from './sections/Hero';
+import ThemeProvider from './context/ThemeProvider';
 
 function App() {
   return (
-    <div className="text-orange-500">
+    <ThemeProvider>
       <Routes>
         <Route path="/" element={ <Hero /> } />
       </Routes>
+    </ThemeProvider>
 
-    </div>
   );
 }
 
