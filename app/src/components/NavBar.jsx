629c0425ef47d6c5e9f14db9c72013dcaf0a31c4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-01 16:30:48 -0300

    refactor :recycle: Mudando cores do tema

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -6,29 +6,33 @@ function NavBar() {
     <nav className="flex space-x-4">
       <Link
         to="/"
-        className="text-[#1b0c44]
-        hover:text-orange-500 px-3 py-2 rounded-md text-sm md:text-lg font-medium"
+        className="text-primary-light dark:text-primary-dark
+        hover:text-secondary-light hover:dark:text-secondary-dark
+          px-3 py-2 rounded-md text-sm md:text-2xl font-medium"
       >
         Início
       </Link>
       <Link
         to="/about"
-        className="text-[#1b0c44]
-         hover:text-orange-500 px-3 py-2 rounded-md text-sm md:text-lg font-medium"
+        className="text-primary-light dark:text-primary-dark
+         hover:text-secondary-light hover:dark:text-secondary-dark
+          px-3 py-2 rounded-md text-sm md:text-2xl font-medium"
       >
         Sobre Nós
       </Link>
       <Link
         to="/services"
-        className="text-[#1b0c44]
-        hover:text-orange-500 px-3 py-2 rounded-md text-sm md:text-lg font-medium"
+        className="text-primary-light dark:text-primary-dark
+        hover:text-secondary-light hover:dark:text-secondary-dark
+         px-3 py-2 rounded-md text-sm md:text-2xl font-medium"
       >
         Serviços
       </Link>
       <Link
         to="/contact"
-        className="text-[#1b0c44]
-        hover:text-orange-500 px-3 py-2 rounded-md text-sm md:text-lg font-medium"
+        className="text-primary-light dark:text-primary-dark
+        hover:text-secondary-light hover:dark:text-secondary-dark
+         px-3 py-2 rounded-md text-sm md:text-2xl font-medium"
       >
         Contatos
       </Link>
